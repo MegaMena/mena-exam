617b8f5f2eb8c6e6af56165e51b32fddbd9c0fe3
MegaMena <guldsmil@hotmail.com>
2018-06-07 15:11:32 +0200

    saveinputs and explanations

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,49 @@ function sendBesked() {
     var besked = document.getElementById("besked");
     besked.className = "show";
     setTimeout(function(){ besked.className = besked.className.replace("show", ""); }, 5000);
-}
 
+    var navn = document.querySelector("#navn")
+    var mail = document.querySelector("#mail")
+    var tele = document.querySelector("#tel")
+    var vedr = document.querySelector("#vedr")
+
+    var newData = [navn.value, mail.value, tele.value, vedr.value];
+
+    var jsonData = JSON.stringify(newData);
+
+    localStorage.setItem("UserData", jsonData);
+};
+
+
+//Variable slideIndex with value 1, this means the default shown slide is slide nr 1
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
+//This function adds a number, assigned in the html, to showDivs's slideIndex, showing of that slide
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
 
+//
 function showDivs(n) {
+  //Standard for writting loops
   var i;
+  //this variable get's its value by getting the element with the class 'mySlides' into an array with 3 'elements'
   var x = document.getElementsByClassName("mySlides");
+  //this gets the value from class 'demo'
   var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  //runs the loops as many times as there are slides
   for (i = 0; i < x.length; i++) {
+    //Every slide gets the styledisplay none:
      x[i].style.display = "none";
   }
+  //gives the class block to slide 1, but with js logic, as it starts with 0
+    x[slideIndex-1].style.display = "block";
+
+//replaces the class from the buttons, from active to none
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex-1].style.display = "block";
+//adds the active class to the shown class
   dots[slideIndex-1].className += " active";
 }
